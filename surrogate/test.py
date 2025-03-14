d5a5bd21a9ff46d3a18a0138e326667e30ee6525
Murdock Palmer Aubry <murdock@mail.cs.toronto.edu>
2025-03-14 18:50:00 -0400

    updated paths

--- surrogate/test.py
+++ surrogate/test.py
@@ -3,14 +3,14 @@ from diffusers import StableDiffusionPipeline
 
 if __name__ == "__main__":
     # Load your trained VAEs and MLPs
-    vae_a = torch.load("path/to/vae_a.pt")
-    vae_b = torch.load("path/to/vae_b.pt")
-    mapping_mlp_a_to_b = torch.load("path/to/mapping_mlp_a_to_b.pt")
-    mapping_mlp_b_to_a = torch.load("path/to/mapping_mlp_b_to_a.pt")
+    vae_a = torch.load("/w/383/murdock/models/vae/sd1.4/dim819_epoch100_time_conditioned.pth")
+    vae_b = torch.load("/w/383/murdock/models/vae/sd1.4-dogtuned/dim819_epoch100_time_conditioned.pth")
+    mapping_mlp_a_to_b = torch.load("/w/383/murdock/models/mlp/sd1.4_to_sd1.4-dogtuned_dim819.pt")
+    mapping_mlp_b_to_a = torch.load("/w/383/murdock/models/mlp/sd1.4-dogtuned_to_sd1.4_dim819.pt")
     
     surrogate_model = SurrogateDiffusionModel(
         base_model_id="CompVis/stable-diffusion-v1-4",
-        finetuned_model_id="path/to/your/finetuned-model",
+        finetuned_model_id="frknayk/dreambooth_training",
         vae_a=vae_a,
         vae_b=vae_b,
         mapping_mlp_a_to_b=mapping_mlp_a_to_b,
